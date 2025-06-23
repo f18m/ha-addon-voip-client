bb3eb8839c65602e2f69f7852664b66c07dff9b4
f18m <francesco.montorsi@gmail.com>
2025-06-24 00:50:21 +0200

    Remove templating for accounts config file

--- rootfs/etc/s6-overlay/s6-rc.d/baresip-init/init.sh
+++ rootfs/etc/s6-overlay/s6-rc.d/baresip-init/init.sh
@@ -5,7 +5,6 @@
 # ==============================================================================
 
 ADDON_CONFIG="/data/options.json"
-BARESIP_ACCOUNTS="/etc/baresip/accounts"
 BARESIP_CONTACTS="/etc/baresip/contacts"
 
 # ==============================================================================
@@ -15,15 +14,6 @@ BARESIP_CONTACTS="/etc/baresip/contacts"
 function log_info() {
     bashio::log.info "baresip-init.sh: $@"
 }
-
-log_info "Configuring baresip..."
-tempio \
-    -conf ${ADDON_CONFIG} \
-    -template /usr/share/tempio/baresip-accounts.tmpl \
-    -out "${BARESIP_ACCOUNTS}"
-
-log_info "Baresip accounts:"
-cat -n $BARESIP_ACCOUNTS
 
 tempio \
     -conf ${ADDON_CONFIG} \
